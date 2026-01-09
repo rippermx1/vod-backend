531be2911d193b4de076dffe97f70ae6a9d16c28
Carlos Valdes <valdescarlos17@gmail.com>
2026-01-09 13:41:02 -0300

    support railway database url

--- app/core/config.py
+++ app/core/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
     @property
     def async_database_url(self) -> str:
         if self.DATABASE_URL:
-            return self.DATABASE_URL
+            return self.DATABASE_URL.replace("postgres://", "postgresql+asyncpg://").replace("postgresql://", "postgresql+asyncpg://")
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
     # JWT
